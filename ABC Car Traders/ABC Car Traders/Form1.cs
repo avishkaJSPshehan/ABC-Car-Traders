e798e5eac247f91b4167febd71ef577c0c92a0e4
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-08-20 19:01:39 +0530

    Login Window setup

--- ABC Car Traders/ABC Car Traders/Form1.cs
+++ ABC Car Traders/ABC Car Traders/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ABC_Car_Traders
 {
-    public partial class Form1 : Form
+    public partial class login_form : Form
     {
-        public Form1()
+        public login_form()
         {
             InitializeComponent();
         }
