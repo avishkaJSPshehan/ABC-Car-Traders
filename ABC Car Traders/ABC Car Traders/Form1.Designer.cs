b782ab9a0f885640b7c723d55e72428c6d0670ad
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-08-22 03:34:31 +0530

    Login Setup

---
+++ ABC Car Traders/ABC Car Traders/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ABC_Car_Traders
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adminBtn = new System.Windows.Forms.Button();
+            this.userBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adminBtn
+            // 
+            this.adminBtn.BackColor = System.Drawing.Color.Crimson;
+            this.adminBtn.FlatAppearance.BorderSize = 0;
+            this.adminBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.adminBtn.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminBtn.Location = new System.Drawing.Point(819, 375);
+            this.adminBtn.Name = "adminBtn";
+            this.adminBtn.Size = new System.Drawing.Size(209, 43);
+            this.adminBtn.TabIndex = 1;
+            this.adminBtn.Text = "Continue as Admin";
+            this.adminBtn.UseVisualStyleBackColor = false;
+            this.adminBtn.Click += new System.EventHandler(this.adminBtn_Click);
+            // 
+            // userBtn
+            // 
+            this.userBtn.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.userBtn.FlatAppearance.BorderSize = 0;
+            this.userBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.userBtn.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userBtn.Location = new System.Drawing.Point(600, 375);
+            this.userBtn.Name = "userBtn";
+            this.userBtn.Size = new System.Drawing.Size(209, 43);
+            this.userBtn.TabIndex = 2;
+            this.userBtn.Text = "Continue as User";
+            this.userBtn.UseVisualStyleBackColor = false;
+            this.userBtn.Click += new System.EventHandler(this.userBtn_Click);
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ABC_Car_Traders.Properties.Resources.home_bg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1121, 604);
+            this.Controls.Add(this.adminBtn);
+            this.Controls.Add(this.userBtn);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ABC Car Traders ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button adminBtn;
+        private System.Windows.Forms.Button userBtn;
+    }
+}
+
---
+++ ABC Car Traders/ABC Car Traders/user_login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABC_Car_Traders
+{
+    public partial class user_login : Form
+    {
+        public user_login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_lb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancle_btn_Click(object sender, EventArgs e)
+        {
+            home home = new home();
+            home.Show();
+            this.Hide();
+        }
+    }
+}
